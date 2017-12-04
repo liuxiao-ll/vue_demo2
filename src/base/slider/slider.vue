2d1a20710179a2aae1aa4fd9ae0a0d9c292307c1
liuxiao--ll <609709432@qq.com>
2017-12-04 17:54:27 +0800

    轮播组件，jsonp获取数据

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot>
+      </slot>
+    </div>
+    <div class="dots">
+      <span class="dot" v-for="(item,index) in dots" :class="{active: currentPageIndex === index}"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  import {addClass} from '../../common/js/dom'
+  export default {
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initDots()
+        this._initSlider()
+        if (this.autoPlay) {
+          this._play()
+        }
+      }, 20)
+    },
+    methods: {
+      _setSliderWidth() {
+        this.children = this.$refs.sliderGroup.children
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop) {
+          width += 2 * sliderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: true,
+          snapThreshold: 0.3,
+          snapSpeed: 400
+        })
+        this.slider.on('scrollEnd', () => {
+          let PageIndex = this.slider.getCurrentPage().pageX
+          this.currentPageIndex = PageIndex
+          if (this.autoPlay) {
+            this._play()
+          }
+        })
+        this.slider.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+          }
+        })
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
+      },
+      _play() {
+        let PageIndex = this.currentPageIndex + 1
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(PageIndex, 0, 400)
+        }, this.interval)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+   @import "../../common/st/slider.styl"
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="recommend">
-    recommend
+  <div class="recommend" ref="recommend">
+    <div ref="scroll" class="recommend-content">
+      <div>
+        <div class="slider-wrapper" ref="sliderWrapper" v-if="recommend.length">
+          <slider>
+            <div v-for="item in recommend">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" alt="">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="loading-container">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
- 
+  import {getRecommend} from '../../api/recommend'
+  import {ERR_OK} from '../../api/config'
+  import slider from '../../base/slider/slider'
+
+  export default{
+    data() {
+      return {
+        recommend: []
+      }
+    },
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommend = res.data.slider
+          }
+        })
+      }
+    },
+    components: {
+      slider
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/st/recommend.styl"
 </style>
