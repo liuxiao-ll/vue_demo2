5ab8ddd3bfb3083e75fcaceea972cd387063d7a3
liuxiao--ll <609709432@qq.com>
2017-12-05 12:09:55 +0800

    lazy-load scroll loading组件开发

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div ref="scroll" class="recommend-content">
+    <scroll class="recommend-content" :data="discList" ref="scroll">
       <div>
         <div class="slider-wrapper" ref="sliderWrapper" v-if="recommend.length">
           <slider>
             <div v-for="item in recommend">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
-              </a>
+                <img @load="loadImage" :src="item.picUrl" class="needsclick">
+              </a> 
             </div>
           </slider>
         </div>
@@ -16,7 +16,7 @@
           <ul>
             <li class="item" v-for="item in discList">
               <div class="icon">
-                <img :src="item.imgurl" width="68" height=“60” alt="">
+                <img v-lazy="item.imgurl" width="68" height=“60” alt="">
               </div>
               <div class="text">
                 <h2 class="name">{{item.creator.name}}</h2>
@@ -26,13 +26,16 @@
           </ul>
         </div>
       </div>
-      <div class="loading-container">
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+  import loading from '../../base/loading/loading'
+  import scroll from '../../base/scroll/scroll'
   import {getRecommend, getDiscList} from '../../api/recommend'
   import {ERR_OK} from '../../api/config'
   import slider from '../../base/slider/slider'
@@ -63,10 +66,18 @@
             console.log(this.discList)
           }
         })
+      },
+      loadImage() {
+        if (!this.checkLoaded) {
+          this.$refs.scroll.refresh()
+          this.checkLoaded = true
+        }
       }
     },
     components: {
-      slider
+      slider,
+      scroll,
+      loading
     }
   }
 </script>
